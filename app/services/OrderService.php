593617db57070419aa7c23b0ff602995e60c06c8
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2024-02-21 13:34:48 +0100

    order calc

--- app/services/OrderService.php
+++ app/services/OrderService.php
@@ -9,37 +9,6 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class OrderService
 {
-
-    private function calculateTotalsOfOrderAndOrderItems(Order $order): Order
-    {
-        $order->orderItems->each(function (OrderItem $orderItem) {
-            $total_price = $orderItem->product->price * $orderItem->quantity;
-            $orderItem->total_price = round($total_price, 2);
-        });
-
-        $total_quantity = $order->orderItems->reduce(
-            fn ($acc, OrderItem $orderItem) => $acc + $orderItem->quantity,
-            0
-        );
-
-        $order->total_quantity = $total_quantity;
-
-        $total_unit_price  = $order->orderItems->reduce(
-            fn ($acc, OrderItem $orderItem) => $acc + $orderItem->product->price,
-            0
-        );
-
-        $order->total_unit_price = round($total_unit_price, 2);
-
-        $total_price = $order->orderItems->reduce(function ($acc, OrderItem $orderItem) {
-            return $acc + ($orderItem->quantity * $orderItem->product->price);
-        }, 0);
-
-        $order->total_price = round($total_price, 2);
-
-        return $order;
-    }
-
     public function getAllFilteredOrders(array $filters)
     {
         $orders = Order::query()
@@ -72,5 +41,40 @@ class OrderService
             throw new ResourceNotFoundException("Order Not Found");
 
         return $this->calculateTotalsOfOrderAndOrderItems($order);
+    }
+
+    /**
+     * Calculates Totals of Order and Order Items and returns the order.
+     * @param \App\Models\Order $order
+     * @return \App\Models\Order
+     */
+    private function calculateTotalsOfOrderAndOrderItems(Order $order): Order
+    {
+        $order->orderItems->each(function (OrderItem $orderItem) {
+            $total_price = $orderItem->product->price * $orderItem->quantity;
+            $orderItem->total_price = round($total_price, 2);
+        });
+
+        $total_quantity = $order->orderItems->reduce(
+            fn ($acc, OrderItem $orderItem) => $acc + $orderItem->quantity,
+            0
+        );
+
+        $order->total_quantity = $total_quantity;
+
+        $total_unit_price  = $order->orderItems->reduce(
+            fn ($acc, OrderItem $orderItem) => $acc + $orderItem->product->price,
+            0
+        );
+
+        $order->total_unit_price = round($total_unit_price, 2);
+
+        $total_price = $order->orderItems->reduce(function ($acc, OrderItem $orderItem) {
+            return $acc + ($orderItem->quantity * $orderItem->product->price);
+        }, 0);
+
+        $order->total_price = round($total_price, 2);
+
+        return $order;
     }
 }
