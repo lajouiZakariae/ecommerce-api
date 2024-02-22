ad4f05dda282ca17927eadadf84edd8fcd01ce71
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2024-02-22 10:58:06 +0100

    order validation

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\OrderStoreRequest;
+use App\Http\Requests\OrderStoreRequest;
 use App\Http\Requests\Admin\OrderUpdateRequest;
 use App\Http\Resources\Admin\OrderResource;
 use App\Models\Order;
-use App\Models\OrderItem;
 use App\Services\OrderService;
 use Illuminate\Http\Response;
-use Spatie\RouteAttributes\Attributes\ApiResource;
 
 /**
  * @group Orders
@@ -35,20 +33,12 @@ class OrderController extends Controller
     /**
      * Store a newly created order in storage.
      *
-     * @param  \App\Http\Requests\Admin\OrderStoreRequest  $request
+     * @param  \App\Http\Requests\OrderStoreRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store()
+    public function store(OrderStoreRequest $request)
     {
-        $data = [
-            "client_id" => 1,
-            "coupon_code_id" => 2,
-            "payment_method_id" => fake()->numberBetween(1, 2),
-            "order_items" => [
-                ["product_id" => 1, "quantity" => 2],
-                ["product_id" => 2, "quantity" => 3],
-            ]
-        ];
+        $data = $request->validated();
 
         return $this->orderService->create($data);
     }
---
+++ app/Http/Requests/OrderStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'client_id' => ['required', 'exists:clients,id'],
+            'coupon_code_id' => ['required', 'exists:coupon_codes,id'],
+            'payment_method_id' => ['required', 'exists:payment_methods,id'],
+
+            'order_items.*.product_id' => ['required', 'exists:products,id'],
+            'order_items.*.quantity' => ['required', 'integer'],
+        ];
+    }
+}
