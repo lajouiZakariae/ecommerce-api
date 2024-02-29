9b3b56a97eabfa52fe6d16c746d11db3b630e1c2
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2024-02-29 11:18:05 +0100

    refactor: remove some empty lines

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::controller(JWTAuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('logout', 'logout');
