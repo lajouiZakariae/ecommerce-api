f320914c28c06a4058d78881b03d3c888d6cb79c
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2024-03-04 12:23:03 +0100

    remove: AuthorizeResourceAccess middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\AuthorizeResourceAccess;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,6 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        'authorize-user' => AuthorizeResourceAccess::class
     ];
 }
